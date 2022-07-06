9d5912999b6b3590f0fcbaa74137587e9bed2601
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-07-06 15:44:45 -0700

    Add more data
    p.s. I had to reclone this repo because when I was committing this last,
    I had a power outage for a few seconds and my desktop lost power as it
    was committing. In the 4 second window the commit command was running,
    my pc lost power. What are the odds.

---
+++ t3-rl/match.py
@@ -0,0 +1,38 @@
+import os
+from tqdm import tqdm
+import json
+
+first_bits = {}
+
+outs = os.listdir("./outs")
+
+for out in tqdm(outs):
+    with open("./outs/" + out) as file:
+        bit = ""
+
+        for _ in range(10_000):
+            bit += file.read(1)
+
+        first_bits[out] = bit
+
+sames = {}
+
+for ref_key, ref_value in first_bits.items():
+
+    for k, v in sames.items():
+        if v["val"] == ref_value:
+            sames[k]["sames"].append(ref_key)
+
+    else:
+        sames[ref_key] = {"val": ref_value, "sames": []}
+
+
+new_out = {}
+
+for k, v in sames.items():
+    if len(v["sames"]) > 0:
+        new_out[k] = v["sames"]
+
+with open("sames_out.json", "w") as file:
+    json_object = json.dumps(new_out, indent=4)
+    file.write(json_object)
