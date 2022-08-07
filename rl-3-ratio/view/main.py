a9a11f63a1d7bdece18574db70fad06201377fed
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-08-06 17:19:22 -0700

    Add images into T3

--- rl-3-ratio/view/main.py
+++ rl-3-ratio/view/main.py
@@ -3,7 +3,34 @@ from matplotlib import pyplot as plt
 
 
 df = pd.read_csv("data/data_locations_of_ones.csv")
+df = df.head(100)
+
+df.plot(x='loc_in_t', y='count', kind='scatter', c="count", colormap='plasma')
+plt.xlabel("Location in T")
+plt.ylabel("Ratio of threes / none threes")
+plt.savefig("lambda_ratio_from_data1_ones_100.png")
+
+
+df = pd.read_csv("data/data_locations_of_ones.csv")
 df = df.head(10000)
 
 df.plot(x='loc_in_t', y='count', kind='scatter', c="count", colormap='plasma')
+plt.xlabel("Location in T")
+plt.ylabel("Ratio of threes / none threes")
 plt.savefig("lambda_ratio_from_data1_ones_10000.png")
+
+df = pd.read_csv("data/data_locations_of_zeros.csv")
+df = df.head(100)
+
+df.plot(x='loc_in_t', y='count', kind='scatter', c="count", colormap='plasma')
+plt.xlabel("Location in T")
+plt.ylabel("Ratio of threes / none threes")
+plt.savefig("lambda_ratio_from_data1_zeros_100.png")
+
+df = pd.read_csv("data/data_locations_of_zeros.csv")
+df = df.head(10000)
+
+df.plot(x='loc_in_t', y='count', kind='scatter', c="count", colormap='plasma')
+plt.xlabel("Location in T")
+plt.ylabel("Ratio of threes / none threes")
+plt.savefig("lambda_ratio_from_data1_zeros_10000.png")
